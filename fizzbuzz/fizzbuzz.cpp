f5c51de51a1886e99e077139f74539f8927489a8
gaunah <christoph@zelinski.de>
2015-07-03 21:13:15 +0200

    final fizzbuzz version

--- fizzbuzz/fizzbuzz.cpp
+++ fizzbuzz/fizzbuzz.cpp
@@ -1,11 +1,22 @@
+#include <boost/algorithm/string.hpp>
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
 #include <string>
+#include <vector>
 
+
+void print(int x, int y, int count){
+	for(int i = 1; i <= count; i++){
+		if(i % x == 0) { std::cout << "F"; }
+		if(i % y == 0) { std::cout << "B"; }
+		if(i % x != 0 && i % y != 0) {std::cout << i; }
+		if(i != count) { std::cout << " "; }
+		else { std::cout << std::endl;}
+	}	
+}
 
 int main(int argc, char* argv[]) {
-	
 	if(argc != 2){
 		std::cerr << "usage: ./fizzbuzz <filename>" << std::endl;
 		return EXIT_FAILURE;
@@ -14,8 +25,10 @@ int main(int argc, char* argv[]) {
 	std::ifstream file(argv[1]); // get filename and open
 	if(file.is_open()){
 		std::string line;
-		while(std::getline(file, line)){
-			std::cout << line << std::endl;	
+		while(std::getline(file, line)){ //read line by line
+			std::vector<std::string> fields;
+			boost::split(fields, line, boost::is_any_of(" ")); //slpit by " "
+			print(atoi(fields[0].c_str()), atoi(fields[1].c_str()), atoi(fields[2].c_str()));
 		}
 		file.close();
 	} else {
