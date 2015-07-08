bdc186a58672af26dccfd4837dd5a9021c776844
gaunah <christoph@zelinski.de>
2015-07-08 21:33:15 +0200

    WIP - get n, p1 and p2 from file

---
+++ bit_positions/bitpos.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include <string>
+
+
+void bitpos(int n, int p1, int p2) {
+	std::cout << "n: " << n << " p1: " << p1 << " p2: " << p2 << std::endl;
+}
+
+int main(int argc, char* argv[]){
+	if(argc != 2){
+		std::cerr << "usage: ./bitpos <filename>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::ifstream file(argv[1]); //get filename and open
+	if(file.is_open()){
+		std::string line, n, p1, p2;
+		while(std::getline(file, line)){ //read line by line
+			std::stringstream ss(line);
+			std::getline(ss, n, ','); //get token
+			std::getline(ss, p1, ',');
+			std::getline(ss, p2, ',');
+			bitpos(atoi(n.c_str()), atoi(p1.c_str()), atoi(p2.c_str()));
+		}
+	} else {
+		std::cerr << "could not open file: " << argv[1] << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
