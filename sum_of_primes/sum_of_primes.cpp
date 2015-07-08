bf4a41d038e9bd42a2d257cc50b321d3e0671944
gaunah <christoph@zelinski.de>
2015-07-08 19:29:38 +0200

    sum of primes final version

---
+++ sum_of_primes/sum_of_primes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+	int i = 1, prime_count = 0, sum = 0;
+	while(prime_count != 1000){
+		i++;
+		int count = 0;
+		for(int k = 2; k < i; k++){
+			if(i % k == 0){ count++; }
+		}
+		if(count == 0){ prime_count++; sum += i; }
+	}
+	std::cout << sum << std::endl;
+	return 0;
+}
