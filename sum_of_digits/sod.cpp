f7af051c75ff81bc586ffb4d93421f2f5e99ba60
gaunah <christoph@zelinski.de>
2015-07-13 21:51:35 +0200

    WIP - get line

---
+++ sum_of_digits/sod.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
+
+
+void const sum(std::string str) {
+	
+}
+
+int main(int argc, char* argv[]){
+	if(argc != 2){
+		std::cerr << "usage: ./sod <filename>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::ifstream file(argv[1]); //get filename and open
+	if(file.is_open()){
+		std::string line;
+		while(std::getline(file, line)){ //read line by line
+			sum(line);
+		}
+	} else {
+		std::cerr << "could not open file: " << argv[1] << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
