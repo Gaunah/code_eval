575489d07f9a90359795ec8d5c852e3484bfc621
gaunah <christoph@zelinski.de>
2015-07-15 15:03:15 +0200

    multiplication tables final version

---
+++ multiplication_tables/mt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+int main(){
+	using namespace std;
+	for(int i = 1; i <= 12; i++){
+		for(int k = 1; k <= 12; k++){
+			cout << setw(4) << right << i*k;
+		}
+		cout << endl;
+	}
+	return 0;
+}
