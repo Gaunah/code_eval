7467e566c7b5dec300a207f1389eb7243946be82
gaunah <christoph@zelinski.de>
2015-11-19 16:56:52 +0100

    WIP - just print given number of lines

--- medium/longest_lines/longest_line.cpp
+++ medium/longest_lines/longest_line.cpp
@@ -1,7 +1,16 @@
+#include <algorithm>
+#include <vector>
 #include <fstream>
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+
+void PrintLongestLines(std::vector<std::string> &lines, int const &count){
+	using namespace std;
+	for(int i = 0; i < count; i++){
+		cout << lines[i] << endl;
+	}
+}
 
 int main(int argc, char* argv[]){
 	if(argc != 2){
@@ -12,9 +21,13 @@ int main(int argc, char* argv[]){
 	std::ifstream file(argv[1]); //get filename and open
 	if(file.is_open()){
 		std::string line, n;
+		std::getline(file, n);
+		int count = std::stoi(n); //how many lines to print
+		std::vector<std::string> lines;
 		while(std::getline(file, line)){ //read line by line
-			//TODO
+			lines.push_back(line);
 		}
+		PrintLongestLines(lines, count);
 	} else {
 		std::cerr << "could not open file: " << argv[1] << std::endl;
 		return EXIT_FAILURE;
